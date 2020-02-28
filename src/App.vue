1db2311d278ba76c8af026baaafeb5a9eea31e37
MrKrasnov <mr.krasnov.y@gmail.com>
2020-02-28 02:41:18 +0200

    Complete markup topmenu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <template>
   <div id="app">
+    <topmenu/>
     <router-view/>
   </div>
 </template>
+<script>
+import Topmenu from './views/TopMenu.vue';
 
+export default {
+  name: "app",
+  components: {
+    Topmenu,
+  }
+}
+</script>
 <style lang="sass">
+.container
+  width: 90%
+  margin: 0 auto
+body
+  margin: 0
+  padding: 0
+  font-family: 'Proxima Nova Rg'
+button
+  border: none
+  background: none
+  cursor: pointer
+  &:hover, &:focus
+    outline: none
 </style>
---
+++ src/views/TopMenu.vue
@@ -0,0 +1,55 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="topmenu">
+                <div class="topmenu-start">
+                    <div class='topmenu-start-component'>
+                        <button class='topmenu-start-component-btn'>
+                            <img class='topmenu-start-component-btn-img' src="../assets/menu.svg" alt="menu-svg">
+                        </button>
+                    </div>
+                    <div class='topmenu-start-component'>
+                        12.12.12
+                    </div>
+                </div>
+                <div>
+                    <button class="topmenu-start-profile">
+                        User Name
+                    </button>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Topmenu',
+}
+</script>
+<style lang="sass">
+header
+    background-color: #89aa94
+.topmenu
+    display: flex
+    justify-content: space-between
+    align-items: center
+    &-start
+        display: flex
+        align-items: center
+        &-profile
+            padding: 25px 30px
+            font-size: 18px
+            &:hover
+                transition: 1s
+                backdrop-filter: opacity(0.90)
+        &-component
+            margin: 0 15px
+            font-size: 20px
+            &-btn
+                width: 50px
+                height: 50px
+                &-img
+                    width: 100%
+                    height: 100%
+</style>
