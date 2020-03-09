dcf3edf7736bc6893ac23fef3f6366d5c1b0cabb
MrKrasnov <mr.krasnov.y@gmail.com>
2020-03-09 03:52:46 +0200

    Added the ability to delete and add a user

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -10,8 +10,22 @@ router.get('/', async (req, res) => {
 });
 
 // Добавляем пользователя
-
+router.post('/', async (req, res) => {
+    const users = await loadUsersCollection();
+    await users.insertOne({
+        login: req.body.login,
+        password: req.body.password
+    });
+    res.status(201).send();
+});
 // Удаляем пользователя
+router.delete('/:id', async (req, res) => {
+    const users = await loadUsersCollection();
+    await users.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+    res.status(200).send();
+});
+
+
 
 async function loadUsersCollection() {
     const client = await mongodb.MongoClient.connect
